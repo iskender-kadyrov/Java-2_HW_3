52361b4fde6f1c438c2d58d433c4ba9ab4783b50
User <kadyroff94@gmail.com>
2021-10-10 15:53:15 +0600

    Final version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,50 +1,28 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws LimitException {
-        // write your code here
-        Scanner scanner = new Scanner(System.in);
-        //Integer depositAmount = scanner.nextInt();
+
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(20000);
         System.out.println(bankAccount.getAmount());
-        String withdrawRemainingAmount;
-        int remainingAmount;
 
         while (true) {
             try {
-                System.out.println("You balance: " + bankAccount.getAmount());
+                System.out.println("You balance is: " + bankAccount.getAmount());
                 bankAccount.withdraw(6000);
                 System.out.println("You withdrew: 6000");
-//                System.out.println("Do you want to withdraw the" + bankAccount.getAmount());
 
             } catch (LimitException limitException) {
                 System.out.println(limitException.getMessage());
                 System.out.println("You may withdraw only: " + bankAccount.getAmount());
-
-//                System.out.println("Would you like to withdraw the remaining amount?");
-//                // boolean withdraw = false;
                 System.out.println("You withdrew remaining amount: " + bankAccount.getAmount());
                 bankAccount.withdraw((int) bankAccount.getAmount());
-                System.out.println("Your balance: " + bankAccount.getAmount());
-//                if (withdraw == true){
-//                    bankAccount.withdraw((int) bankAccount.getAmount());
-//                    System.out.println("Remaining balance: " + bankAccount.getAmount());
-//                }
-
+                System.out.println("Your balance is: " + bankAccount.getAmount());
                 break;
             }
-//            if (scanner.nextLine() == "y") {
-//                remainingAmount = (int) bankAccount.getAmount();
-//                bankAccount.withdraw(remainingAmount);
-//                System.out.println("You withdrew: " + remainingAmount);
-//            }
-//            if (scanner.nextLine() == "n") {
-//                System.out.println("Your balance is: " + bankAccount.getAmount());
-//            } else System.out.println("Your balance is: " + bankAccount.getAmount());
         }
     }
 }
